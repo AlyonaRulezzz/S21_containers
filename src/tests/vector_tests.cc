7aa0d1b3e512f79c44130afcfe8755805003a27c
Milk Fist <milkfist@si-n6.kzn.21-school.ru>
2022-11-23 01:15:42 +0300

    copy constructor test

--- src/tests/vector_tests.cc
+++ src/tests/vector_tests.cc
@@ -18,6 +18,7 @@ TEST(Constructor, With_parameter) {
 TEST(Constructor, Initializer_list) {
   std::vector<double> v1 = {1, 2, 3, 4.356, 5.0};
   s21::Vector<double> v2 = {1, 2, 3, 4.356, 5.0};
+
   for (size_t i = 0; i < v1.size(); ++i) {
     EXPECT_EQ(v1[i], v2[i]);
   }
@@ -25,19 +26,14 @@ TEST(Constructor, Initializer_list) {
   EXPECT_EQ(v1.capacity(), v2.capacity());
 }
 
-// // TEST(Constructor, Copy) {
-// //   s21::Vector<int> test1;
-// //   test1.Fill(5);
+TEST(Constructor, Copy) {
+  s21::Vector<double> v2 = {1, 2, 3, 4.356, 5.0};
+  s21::Vector<double> v1(v2);
 
-// //   s21::Vector<int> m1(test1);
-// //   EXPECT_EQ(test1.GetSize(), m1.GetSize());
-// //   EXPECT_EQ(test1.GetCapacity(), m1.GetCapacity());
-// //   for (int i = 0; i < m1.GetSize(); i++) {
-// //     for (int j = 0; j < m1.GetCapacity(); j++) {
-// //       EXPECT_EQ(test1(i, j), m1(i, j));
-// //     }
-// //   }
-// // }
+  for (size_t i = 0; i < v1.size(); ++i) {
+    EXPECT_EQ(v1.at(i), v1.at(i));
+  }
+}
 
 // // TEST(Constructor, Move) {
 // //   s21::Vector<int> test1(4, 5);
@@ -53,6 +49,7 @@ TEST(Constructor, Initializer_list) {
 TEST(vector_element, at) {
     std::vector<double> v1 = {1.366, 2, 3};
     s21::Vector<double> v2 = {1.366, 2, 3};
+
     for (size_t i = 0; i < v1.size(); ++i) {
         EXPECT_EQ(v1.at(i), v2.at(i));
     }
@@ -63,6 +60,7 @@ TEST(vector_element, at) {
 TEST(vector_element, brackets) {
     std::vector<double> v1 = {1.366, 2, 3};
     s21::Vector<double> v2 = {1.366, 2, 3};
+    
     for (size_t i = 0; i < v1.size(); ++i) {
         EXPECT_EQ(v1[i], v2[i]);
     }
@@ -71,16 +69,19 @@ TEST(vector_element, brackets) {
 TEST(vector_element, front_and_back) {
     std::vector<double> v1 = {1.366, 2, 3};
     s21::Vector<double> v2 = {1.366, 2, 3};
+
     EXPECT_EQ(v1.front(), v2.front());
     EXPECT_EQ(v1.back(), v2.back());
 }
 
 TEST(vector_element, data) {
   s21::Vector<int> v1;
+
   EXPECT_EQ(v1.data(), nullptr);
 
   std::vector<double> v2 = {1.366, 2, 3, 4, 5};
   s21::Vector<double> v3 = {1.366, 2, 3, 4, 5};
+
   EXPECT_EQ(*v2.data(), *v3.data());
 }
 
@@ -89,6 +90,7 @@ TEST(vector_iterator, begin) {
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
   auto iter1 = v1.begin();
   auto iter2 = v2.begin();
+
   EXPECT_EQ(*iter1, *iter2);
 }
 
@@ -97,28 +99,33 @@ TEST(vector_iterator, end) {
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
   auto iter1 = v1.end();
   auto iter2 = v2.end();
+
   EXPECT_EQ(*(iter1 - 1), *(iter2 - 1));
 }
 
 TEST(Vector_capacity, empty) {
   std::vector<int> v1 = {1, 2, 3, 4, 5};
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
+
   EXPECT_EQ(v1.empty(), v2.empty());
 
   std::vector<int> v3 = {};
   s21::Vector<int> v4 = {};
+
   EXPECT_EQ(v3.empty(), v4.empty());
 }
 
 TEST(Vector_capacity, size) {
   std::vector<int> v1 = {1, 2, 3, 4, 5};
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
+
   EXPECT_EQ(v1.size(), v2.size());
 }
 
 TEST(Vector_capacity, max_size) {
   std::vector<int> v1 = {1, 2, 3, 4, 5};
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
+
   EXPECT_EQ(v1.max_size(), v2.max_size());
 }
 
@@ -127,11 +134,13 @@ TEST(Vector_capacity, reserve_and_capacity) {
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
   v1.reserve(30);
   v2.reserve(30);
+
   EXPECT_EQ(v1.size(), v2.size());
   EXPECT_EQ(v1.capacity(), v2.capacity());
 
   v1.reserve(0);
   v2.reserve(0);
+
   EXPECT_EQ(v1.size(), v2.size());
   EXPECT_EQ(v1.capacity(), v2.capacity());
 
@@ -143,23 +152,28 @@ TEST(Vector_capacity, shrink_to_fit) {
   s21::Vector<int> v2 = {1, 2, 3, 4, 5};
   v1.shrink_to_fit();
   v2.shrink_to_fit();
+
   EXPECT_EQ(v1.size(), v1.capacity());
   EXPECT_EQ(v2.size(), v2.capacity());
 }
 
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
 
 
 
 ///////////////////////////////
-// #include <iostream>
+#include <iostream>
 
-// int main() {
-//   // std::vector<double> vec = {1, 2, 3, 4, 5};
-//   s21::Vector<double> vec = {1, 2, 3, 4, 5};
-//   std::cout << vec.max_size();
-// }
+int main() {
+  s21::Vector<double> v2 = {1, 2, 3, 4.356, 5.0};
+  s21::Vector<double> v1(v2);
+
+  for (size_t i = 0; i < v1.size(); ++i) {
+    EXPECT_EQ(v1.at(i), v1.at(i));
+    std::cout << v1.at(i) << std::endl;
+  }
+}
